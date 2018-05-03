384f75128012dc41b6fa85099448c7e935cfed62
deadman96385 <seanhoyt963@gmail.com>
2018-05-02 19:29:34 -0500

    Add wacom files, lenovo_sn

---
+++ proprietary/etc/wacom_update.sh
@@ -0,0 +1,3 @@
+#!/system/bin/sh
+/system/bin/logwrapper /system/bin/wac_flash_w9013 /system/etc/firmware/W9013_0333.hex
+/system/bin/logwrapper /system/bin/wac_flash /system/etc/firmware/W9002_1428.hex
